1b0e8054f43cc7cc15c789b803ddded8658f3595
creations <creations@creations.works>
2025-01-05 21:47:27 -0500

    edit env

--- config/environment.ts
+++ config/environment.ts
@@ -11,7 +11,9 @@ try {
 export const environment: Environment = {
 	port: parseInt(process.env.PORT || "6600", 10),
 	host: process.env.HOST || "0.0.0.0",
-	development: process.env.NODE_ENV === "development",
+	development:
+		process.env.NODE_ENV === "development" ||
+		process.argv.includes("--dev"),
 };
 
 export const redisConfig: RedisConfig = {
